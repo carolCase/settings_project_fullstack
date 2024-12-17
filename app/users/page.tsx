e1c639120bc03511c7ee932de7bff8304d228f6b
Carolina Castro <144167622+carolCase@users.noreply.github.com>
2024-12-17 11:05:39 +0100

    Add- delete button to erase user

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -41,6 +41,15 @@ export default function User() {
   }, [])
 
   const handleCreateUser = async () => {
+    if (newUser.name.length < 2 || newUser.name.length > 16) {
+      alert("Name must be between 2 and 16 characters")
+      return
+    }
+
+    if (newUser.password.length < 3 || newUser.password.length > 6) {
+      alert("Password must be between 3 to 6 characters")
+    }
+
     const randomImage =
       imageLibrary[Math.floor(Math.random() * imageLibrary.length)]
     const userWithAvatar = {
@@ -61,6 +70,33 @@ export default function User() {
       setNewUser({ name: "", password: "", avatar: "", isAdmin: false })
     } catch (err) {
       console.error("Error during user creation:", err)
+    }
+  }
+
+  const handleDeleteUser = async (id: number) => {
+    const targetUser = users.find((u) => u.id === id)
+    if (!targetUser) return
+
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete ${targetUser.name}? This action cannot be undone.`
+    )
+    if (!confirmDelete) return
+
+    try {
+      const response = await fetch("http://localhost:8080/api/users/${id}", {
+        method: "DELETE",
+      })
+      if (!response.ok) {
+        throw new Error("Failed to delete user, please try again")
+      }
+      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id))
+      console.log("User ${targetUser.name} has been deleted.")
+      alert("User ${targetUser.name} has been successsfully deleted.")
+    } catch (error) {
+      console.error("Error deleting user:", error)
+      alert(
+        "An error occurred while deleting the user, please try again later."
+      )
     }
   }
 
@@ -93,15 +129,15 @@ export default function User() {
     <div className="pt-20">
       <div className="mb-6 p-4 bg-white shadow rounded-lg">
         <h2 className="text-xl font-medium text-gray-600 mb-4">
-          Create New User
+          Add House Users
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
           <input
             type="text"
             placeholder="Name"
             value={newUser.name}
             onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
-            className="p-2 border rounded text-black bg-gray-300"
+            className="p-2 border border-gray-300 rounded-lg text-gray-600 bg-gray-300 shadow-lg "
           />
           <input
             type="text"
@@ -110,43 +146,42 @@ export default function User() {
             onChange={(e) =>
               setNewUser({ ...newUser, password: e.target.value })
             }
-            className="p-2 border rounded text-black bg-gray-300"
-          />
-          <input
-            type="text"
-            placeholder="Avatar URL"
-            value={newUser.avatar}
-            onChange={(e) => setNewUser({ ...newUser, avatar: e.target.value })}
-            className="p-2 border rounded text-black bg-gray-300"
+            className="p-2 border border-gray-300 rounded-lg text-gray-600 bg-gray-300 shadow-lg "
           />
           <button
             onClick={handleCreateUser}
-            className="btn glass mt-4 bg-cyan-950 text-white py-2 px-4 rounded hover:bg-blue-900"
+            className="btn glass mt-4 w-45 bg-cyan-950 text-white py-2 px-4 rounded-lg hover:bg-cyan-700"
           >
-            Create User
+            Create New User
           </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {users.map((user) => (
             <div
               key={user.id}
-              className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center"
+              className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-col items-center"
             >
               <Image
                 src={user.avatar || "/predator.jpg"}
-                alt={user.name}
+                alt={user.name ? `${user.name}'s avatar` : "Default avatar"}
                 width={100}
                 height={100}
                 className="rounded-full mb-4"
               />
-              <h3 className="text-lg font-medium">{user.name}</h3>
+              <h3 className="text-lg font-medium text-gray-700">{user.name}</h3>
               <button
                 onClick={() => handleSetAdmin(user.id)}
                 className={`mt-4 py-2 px-4 rounded ${
-                  user.isAdmin ? "bg-red-800" : "bg-green-500"
+                  user.isAdmin ? "bg-gray-900" : "bg-cyan-800"
                 } text-white`}
               >
-                {user.isAdmin ? "Nullify Admin" : "Make Admin"}
+                {user.isAdmin ? "Remove Admin" : "Make Admin"}
+              </button>
+              <button
+                onClick={() => handleDeleteUser(user.id)}
+                className="mt-2 py-2 px-4 rounded bg-red-600 text-white hover:bg-red-700"
+              >
+                Delete User
               </button>
             </div>
           ))}
