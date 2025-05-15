34b2d63cca0a657405ade0c692b6276280dfe76f
Carolina Castro <144167622+carolCase@users.noreply.github.com>
2025-05-15 18:07:32 +0700

    Feat-sidebars

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import Button from "./components/button"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return (
-    <div className=" w-full h-screen flex flex-col items-center justify-center  space-y-6 bg-cover ">
-      <h2 className="text-5xl text-center text-slate-950 italic pb-6">
-        Welcome to Home Settings
-      </h2>
-      <Button />
-    </div>
-  )
+  redirect("/login")
 }
